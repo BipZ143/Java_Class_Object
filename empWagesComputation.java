1acbf20329970186557a9a726ad56d59a8fb7d14
BipZ143 <sahoobhanupratap2@gmail.com>
2020-08-21 01:33:12 +0530

    Employee attendance

---
+++ empWagesComputation.java
@@ -0,0 +1,12 @@
+public class empWagesComputation{
+        public static void main(String[] args){
+                //Constant
+                int IS_PRESENT=1;
+                int randomCheck=(int)(Math.floor(Math.random()*10)%2);
+                System.out.println(randomCheck);
+                if(IS_PRESENT == randomCheck)
+                        System.out.println("Employee is Present");
+                else
+                        System.out.println("Employee is Absent");
+        }
+}
